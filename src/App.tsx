389a9552a338769512818740f24ae3c8130d8ce5
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-11-24 17:10:27 -0600

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import SocialProof from './components/SocialProof'
+
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Hero />
+      <Features />
+      <SocialProof />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
