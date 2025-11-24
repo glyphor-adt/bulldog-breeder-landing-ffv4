7415e154945ca39a5d98451b4fa14711c1d2aad9
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-11-24 17:10:30 -0600

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="py-8 text-center text-gray-500">
+      <p>&copy; 2024 English Bulldog Breeding. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
